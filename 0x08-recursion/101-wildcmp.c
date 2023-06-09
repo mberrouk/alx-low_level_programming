012b332c5d86a2db95c14d0032393641460e3d20
mberrouk <mberrouk@student.1337.ma>
2023-06-08 18:24:13 -0700

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -76,10 +76,9 @@ int wildcmp(char *s1, char *s2)
 					if (wildcmp(tmp, s2))
 						return (1);
 			}
-			if (*s2 != *s1)
-				s1 = skip_stars(s1, *s2, _strlen(s1) - 1);
-				if (!s1)
-					return (0);
+			s1 = skip_stars(s1, *s2, _strlen(s1) - 1);
+			if (!s1)
+				return (0);
 		}
 		else
 		{
