aa889c602a80cc48d1bfb751d8d7e3cc43bec3b8
mberrouk <mberrouk@student.1337.ma>
2023-06-09 14:32:44 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * main -  program that prints its name, followed by a new line
+ * @ac: The number of arguments passed to the program
+ * @av: An array of strings containing the arguments passed to the program
+ *
+ * Return: Always 0
+ */
 int main(int ac, char *av[])
 {
 	int i;
@@ -7,4 +14,5 @@ int main(int ac, char *av[])
 	for (i = 0; av[0][i]; i++)
 		write(STDOUT_FILENO, &av[0][i], 1);
 	write(STDOUT_FILENO, "\n", 1);
+	return (0);
 }
