04a3bd802acc7fd082c9b51551c24a835cd12dea
mberrouk <mberrouk@student.1337.ma>
2023-06-09 14:44:02 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * ft_putnbr - Prints an integer to standard output
+ * @n: The integer to be printed
+ */
+void ft_putnbr(int n)
+{
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	write(STDOUT_FILENO, &"0123456789"[n % 10], 1);
+}
+
+/**
+ * main - Prints the number of arguments passed to the program
+ * @ac: The number of arguments passed to the program
+ *
+ * Return: Always 0
+ */
+int main(int ac)
+{
+	ft_putnbr(ac);
+	write(STDOUT_FILENO, "\n", 1);
+	return (0);
+}
