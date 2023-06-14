04368a9a563b56bc116b500c6d089fcbd4ad727d
mberrouk <mberrouk@student.1337.ma>
2023-06-14 15:38:50 -0700

    script

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,16 @@
+Copy
+
+#!/bin/bash
+
+# Compile all .c files in the current directory into object files
+gcc -Wall -Werror -Wextra -pedantic -c *.c
+
+# Create a static library from the object files
+ar -rc liball.a *.o
+
+# Index the symbols in the library for faster linking
+#ranlib liball.a
+
+# Clean up the object files
+rm *.o
+
