c6bc27bcdade80d57780cb90dc5331424d902459
mberrouk <mberrouk@student.1337.ma>
2023-06-14 16:05:13 -0700

    init

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,1 +1,24 @@
 #include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to search
+ * @accept: the string containing the characters to match
+ *
+ * Return: a pointer to the byte in s that matches one of thebyte
+ * bytes in accept, or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+	return (NULL);
+}
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,1 +1,13 @@
 #include "main.h"
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: the character to write
+ *
+ * Return: the number of characters written (always 1).
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
