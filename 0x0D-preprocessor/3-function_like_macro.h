a45fb39b06d9f70a424b377b7da904b94c2ece32
mberrouk <mberrouk@student.1337.ma>
2023-06-20 12:15:22 -0700

    3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,8 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
 #ifndef ABS
-#define ABS(x) (x < 0 ? -x : x)
+#define ABS(x) (((x) < (0)) ? (-x) : (x))
 #endif
 
 #endif
