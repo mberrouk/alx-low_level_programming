5e653a24e4f07ae27a81fd74229413e4d471f9a5
mberrouk <mberrouk@student.1337.ma>
2023-06-20 13:10:55 -0700

    add_2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - Represents a dog
  * @name: The name of the dog
@@ -18,5 +21,6 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
