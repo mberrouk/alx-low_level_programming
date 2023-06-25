71a0e3271ac4d7df39b0b212aaf7fe47c37971db
mberrouk <mberrouk@student.1337.ma>
2023-06-25 10:14:05 -0700

    init

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,85 @@
+#include "variadic_functions.h"
+
+/**
+ * ft_putchar - Prints a character
+ * @va: The va_list containing the character to be printed
+ *
+ * Return: void
+ */
+void ft_putchar(va_list va)
+{
+	printf("%c", (char)va_arg(va, int));
+}
+
+/**
+ * ft_putstr - Prints a string
+ * @va: The va_list containing the string to be printed
+ *
+ * Return: void
+ */
+void ft_putstr(va_list va)
+{
+	printf("%s", va_arg(va, char *));
+}
+
+/**
+ * ft_putint - Prints an integer
+ * @va: The va_list containing the integer to be printed
+ *
+ * Return: void
+ */
+void ft_putint(va_list va)
+{
+	printf("%i", va_arg(va, int));
+}
+
+/**
+ * ft_putfloat - Prints a float
+ * @va: The va_list containing the float to be printed
+ *
+ * Return: void
+ */
+void ft_putfloat(va_list va)
+{
+	printf("%f", va_arg(va, double));
+}
+
+/**
+ * print_all - Prints a variable number of arguments based on the format string
+ * @format: The format string specifying the types of the arguments
+ * @...: Variable arguments to be printed
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	int i;
+	int j;
+	va_list va;
+	t_form chois[] = {
+		{'c', ft_putchar},
+		{'s', ft_putstr},
+		{'i', ft_putint},
+		{'f', ft_putfloat}
+	};
+	i = 0;
+	va_start(va, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == chois[j].form)
+			{
+				chois[j].func(va);
+				break;
+			}
+			j++;
+		}
+		if (format[i + 1] && j < 4)
+			printf(", ");
+		i++;
+	}
+	va_end(va);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,8 +4,21 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct s_form - Structure representing a format
+ * specifier and its corresponding function
+ * @form: The format specifier character
+ * @func: Pointer to the function associated with the format specifier
+ */
+typedef struct s_form
+{
+	char form;
+	void (*func)(va_list va);
+}	t_form;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
