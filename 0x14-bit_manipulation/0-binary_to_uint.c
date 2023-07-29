a31a9e26f41a2e55fe0eeac60e502c6239063f7c
mberrouk <mberrouk@student.1337.ma>
2023-07-28 17:03:55 -0700

    init

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,12 +13,12 @@ unsigned int binary_to_uint(const char *b)
 	int len;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 
 	for (len = 0; b[len]; len++)
 	{
 		if (b[len] != '0' && b[len] != '1')
-			return 0;
+			return (0);
 	}
 	for (len--; len >= 0; len--, power *= 2)
 	{
