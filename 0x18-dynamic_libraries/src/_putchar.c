b9f29ba9bd6f4b3120555b4d7ea2fbc859881dba
mberrouk <mberrouk@student.1337.ma>
2023-08-21 15:06:58 -0700

    add _putchar

---
+++ 0x18-dynamic_libraries/src/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: the character to write
+ *
+ * Return: the number of characters written (always 1).
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
