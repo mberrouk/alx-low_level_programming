f405001f6ef92edc030d3f7d75dbadf3c9555a58
mberrouk <mberrouk@student.1337.ma>
2024-02-07 08:39:02 -0800

    print the array being searched every time it changes.

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,12 +13,14 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t left = 0;
 	size_t right = size - 1;
-	size_t middle;
+	size_t middle, i;
 
 	if (!array || !size)
 		return (-1);
 	while (left <= right)
 	{
+		for (i = left, printf("Searching in array: "); i <= right; i++)
+			printf("%d%s", array[i], i < right ? ", " : "\n");
 		if (left == right && array[left] == value)
 			return (left);
 		else if (left == right)
